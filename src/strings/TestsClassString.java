155e98879a733f02f887f7a3645a5d5d102cc72d
Kirillgutsalo <kirya.sever11@gmail.com>
2021-12-12 00:45:58 +0200

    adds commit with hw#19

--- src/strings/TestsClassString.java
+++ src/strings/TestsClassString.java
@@ -6,104 +6,141 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TestsClassString {
-
+final static String str = "I learn Java";
 	@BeforeEach
 	void setUp() throws Exception {
 	}
 
 	@Test
 	void testCharAt() {
-		//TODO
+		assertEquals('I', str.charAt(0));
+		assertEquals('J', str.charAt(8));
 	}
 
 	@Test
 	void testEqualsIgnoreCase() {
-		//TODO
+		String str1 = "I learn Java";
+		String str2 = "I LEARN JAVA";
+		String str3 = "I LEARN JAV";
+		assertEquals(true, str.equalsIgnoreCase(str1));
+		assertEquals(true, str.equalsIgnoreCase(str2));
+		assertEquals(false, str.equalsIgnoreCase(str3));
 	}
 
 	@Test
 	void testCompareToIgnoreCase() {
-		//TODO
+		String str1 = "I learn Java";
+		String str2 = "I LEARN JAVA";
+		String str3 = "I lear Java";
+		assertEquals(true, str.equalsIgnoreCase(str1));
+		assertEquals(true, str.equalsIgnoreCase(str2));
+		assertEquals(false, str.equalsIgnoreCase(str3));
 	}
 
 	@Test
 	void testStartsWithStringInt() {
-		//TODO
+		assertEquals(true, str.startsWith("I l", 0));
+		assertEquals(true, str.startsWith("Ja", 8));
+		assertEquals(false, str.startsWith("I", 1));
 	}
 
 	@Test
 	void testStartsWithString() {
-		//TODO
+		assertEquals(true, str.startsWith("I"));
+		assertEquals(true, str.startsWith("I le"));
+		assertEquals(false, str.startsWith("va"));
 	}
 
 	@Test
 	void testEndsWith() {
-		//TODO
+		assertEquals(true, str.endsWith("Java"));
+		assertEquals(true, str.endsWith("a"));
+		assertEquals(false, str.endsWith("J"));
 	}
 
 	@Test
 	void testIndexOfInt() {
-		//TODO
+		assertEquals(0, str.indexOf(73));
+		assertEquals(-1, str.indexOf("Y"));
+		assertEquals(8, str.indexOf(74));
 	}
 
 	@Test
 	void testIndexOfIntInt() {
-		//TODO
+		assertEquals(-1, str.indexOf("j", 8));
+		assertEquals(8, str.indexOf("J", 8));
+		assertEquals(0, str.indexOf("I", 0));
 	}
 
 	@Test
 	void testLastIndexOfIntInt() {
-		//TODO
+		assertEquals(11, str.lastIndexOf("a", 11));
+		assertEquals(4, str.lastIndexOf("a", 8));
+		assertEquals(-1, str.lastIndexOf("N", 10));
 	}
 
 	@Test
 	void testIndexOfString() {
-		//TODO
+		assertEquals(2, str.indexOf("l"));
+		assertEquals(0, str.indexOf("I"));
+		assertEquals(-1, str.indexOf("H"));
 	}
 
 	@Test
 	void testLastIndexOfString() {
-		//TODO
+		assertEquals(7, str.lastIndexOf(" "));
+		assertEquals(11, str.lastIndexOf("a"));
+		assertEquals(-1, str.lastIndexOf("!"));
 	}
 
 	@Test
 	void testSubstringInt() {
-		//TODO
+		assertEquals("Java", str.substring(8));
+		assertEquals("va", str.substring(10));
 	}
 
 	@Test
 	void testSubstringIntInt() {
-		//TODO
+		assertEquals("Java", str.substring(8, 12));
+		assertEquals("learn", str.substring(2, 7));
+		
 	}
 
 	@Test
 	void testContains() {
-		//TODO
+		assertTrue(str.contains("Java"));
+		assertFalse(str.contains("Learn"));
 	}
 
 	@Test
 	void testJoinCharSequenceCharSequenceArray() {
-		//TODO
+		String[] str1 = {"Hello", "my", "name", "is", "Kirill"};
+		assertEquals("Hello my name is Kirill", str.join(" ", str1));
 	}
 
 	@Test
 	void testToLowerCase() {
-		//TODO
+		assertEquals("i learn java", str.toLowerCase());
 	}
 
 	@Test
 	void testTrim() {
-		//TODO
+		String str1 = " London is a capital of Great Britain ";
+		assertEquals("London is a capital of Great Britain", str1.trim());
 	}
 
 	@Test
 	void testToCharArray() {
-		//TODO
+		String str1 = "Tel-Ran";
+		char[] expected = {'T', 'e', 'l', '-', 'R', 'a', 'n'};
+		assertArrayEquals(expected, str1.toCharArray());
 	}
 
 	@Test
 	void testFormatStringObjectArray() {
-		//TODO
+		String str1 = "I'm from %s-%s-%s";
+		String[] obj = {"Rostov", "on", "Don"};
+		assertEquals("I'm from Rostov-on-Don", str1.format(str1, obj));
 	}
 
 }
--- src/strings/TestsClassStringBuilder.java
+++ src/strings/TestsClassStringBuilder.java
@@ -5,25 +5,28 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class TestsClassStringBuilder {
-
+final StringBuilder strBuilder = new StringBuilder("I learn");
 	@Test
 	void testAppendString() {
-		//TODO
+		String str = " Java!";
+		String expected = "I learn Java!";
+		assertEquals(expected, strBuilder.append(str).toString());
 	}
 
 	@Test
 	void testReplaceIntIntString() {
-		//TODO
+		assertEquals("I study", strBuilder.replace(2, 7, "study").toString());
 	}
 
 	@Test
 	void testInsertIntString() {
-		//TODO
+		assertEquals("I am learn", strBuilder.insert(2, "am ").toString());
 	}
 
 	@Test
 	void testReverse() {
-		//TODO
+		StringBuilder stringBuilder1 = new StringBuilder("123456789");
+		assertEquals("987654321", stringBuilder1.reverse().toString());
 	}
 
 }
